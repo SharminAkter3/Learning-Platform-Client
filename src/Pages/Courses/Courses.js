2b680e9cfc196a30dc25744eb3bbac3408ac5ade
SharminAkter3 <mssharmin4145@gmail.com>
2022-10-25 08:19:39 -0700

    added-login-page

--- src/Pages/Courses/Courses.js
+++ src/Pages/Courses/Courses.js
@@ -17,12 +17,17 @@ const Courses = () => {
             <div>
                 <h2 className='fs-4 text-primary m-3 p-3 d-flex justify-content-center'>All Courses : {courses.length}</h2>
             </div>
-            <div>
-                {
-                    courses.map(course => <p key={course.id}>
-                        <Link to={`/courses/${course.id}`}>{course.name}</Link>
-                    </p>)
-                }
+            <div className='d-flex'>
+                <div className='fs-4 ms-4 p-2'>
+                    {
+                        courses.map(course => <p key={course.id}>
+                            <Link to={`/courses/${course.id}`}>{course.name}</Link>
+                        </p>)
+                    }
+                </div>
+                <div>
+
+                </div>
             </div>
         </div>
     );
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { FaGoogle, FaGithub, FaEnvelopeSquare } from 'react-icons/fa'
 
 const Login = () => {
     return (
         <div>
             <h2>login page</h2>
+            <ButtonGroup vertical>
+                <Button className='mb-2' variant='outline-primary'><FaGoogle></FaGoogle> Login With Google</Button>
+                <Button className='mb-2' variant='outline-dark'><FaGithub></FaGithub> Login With Github</Button>
+                <Button className='mb-2' variant='outline-secondary'><FaEnvelopeSquare></FaEnvelopeSquare> Login With Email</Button>
+            </ButtonGroup>
         </div>
     );
 };
