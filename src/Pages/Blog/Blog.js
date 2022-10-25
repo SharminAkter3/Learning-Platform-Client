b183674f8a5c5ea9472da3789ba1d138d4699137
SharminAkter3 <mssharmin4145@gmail.com>
2022-10-25 05:57:01 -0700

    added-third-question

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -9,11 +9,11 @@ const Blog = () => {
             <h4>4.2 Why are you using firebase? What other options do you have to implement authentication? </h4>
             <p> Firebase provides detailed documentation and cross-platform SDKs to help you build and ship apps on Android, iOS, the web, C++, and Unity. Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more. </p>
 
-            {/* <h4>4.3 How does the private route work?</h4>
+            <h4>4.3 How does the private route work?</h4>
             <p>The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).  If not logged in the user is redirected to the /login page with the return url passed in the location state property.</p>
-
+            {/* 
             <h4>4.4 What is Node? How does Node work?</h4>
-            <p> A node is a point of intersection/connection within a data communication network. In an environment where all devices are accessible through the network, these devices are all considered nodes. The individual definition of each node depends on the type of network it refers to. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.</p>  */}
+            <p> A node is a point of intersection/connection within a data communication network. In an environment where all devices are accessible through the network, these devices are all considered nodes. The individual definition of each node depends on the type of network it refers to. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.</p>   */}
         </div>
 
     );
