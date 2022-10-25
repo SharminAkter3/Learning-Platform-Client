0704c282b14616612c7a14f1c1198e9621e2697f
SharminAkter3 <mssharmin4145@gmail.com>
2022-10-25 06:26:00 -0700

    added-error-page

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div className='fs-2 m-4 p-4 fw-bold d-flex align-middle justify-content-center'>
-            {/* <h1>Oops!!! An Error Occured !!!</h1> */}
+            <h1>Oops!!! An Error Occured !!!</h1>
         </div>
     );
 };
