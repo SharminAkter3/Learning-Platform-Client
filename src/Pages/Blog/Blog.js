219b6a202ca4bb0bf03e76c22e959907bcd5d760
SharminAkter3 <mssharmin4145@gmail.com>
2022-10-25 05:55:47 -0700

    added-second-question

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -5,11 +5,11 @@ const Blog = () => {
         <div>
             <h4>4.1 What is cors?</h4>
             <p> Cross-Origin Resource Sharing (CORS) is an HTTP-based security mechanism controlled and enforced by the client (web browser). It allows a service (API) to indicate any origin other than its own from which the client can request resources. It has been designed in response to the same-origin policy (SOP) that restricts how a website (HTML document or JS script) loaded by one origin can interact with a resource from another origin. CORS is used to explicitly allow some cross-origin requests while rejecting others. </p>
-            {/* 
+
             <h4>4.2 Why are you using firebase? What other options do you have to implement authentication? </h4>
             <p> Firebase provides detailed documentation and cross-platform SDKs to help you build and ship apps on Android, iOS, the web, C++, and Unity. Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more. </p>
 
-            <h4>4.3 How does the private route work?</h4>
+            {/* <h4>4.3 How does the private route work?</h4>
             <p>The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in).  If not logged in the user is redirected to the /login page with the return url passed in the location state property.</p>
 
             <h4>4.4 What is Node? How does Node work?</h4>
